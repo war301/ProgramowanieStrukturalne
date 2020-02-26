62229bc96e1f35110d965cad0329fe7099cd4c54
war301 <58977515+war301@users.noreply.github.com>
2020-02-26 14:16:43 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    printf("Bardzo\n");
+    printf("d³ugi\n");
+    printf("tekst\n");
+        return 0;
+}
