fac1a90320be938301a6567d3fdaeb24616ee142
war301 <58977515+war301@users.noreply.github.com>
2020-02-26 15:25:39 +0100

    Add files via upload

---
+++ ćw7/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,c;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+    float cos= (float)(a+b+c)/3;
+    printf("%f",cos);
+    return 0;
+}
---
+++ ćw9/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    float a;
+    scanf("%f",&a);
+    printf("%f",fabs(a)); //bezwzgledna
+    return 0;
+}
